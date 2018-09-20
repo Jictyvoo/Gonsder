e00ac981bcbc4bd7fdaefd19b4f9a7205cf7d392
Jictyvoo <jictyvoo.ecomp@gmail.com>
2018-09-20 19:33:10 -0300

    Minimal Changes

--- src/os_check_use.h
+++ src/os_check_use.h
@@ -2,31 +2,24 @@
 #define OS_CHECK_USE_H*/
 
 #include <stdio.h>
-#if _WIN32	/*verifica qual o sistema operacional para definir o tipo de comando a usar*/
-#define SISTEMAOPERACIONAL 1
+
+#if _WIN32    /*verifica qual o sistema operacional para definir o tipo de comando a usar*/
+#define OPERATIONAL_SYSTEM 1
 #else
-#define SISTEMAOPERACIONAL 0
+#define OPERATIONAL_SYSTEM 0
 #endif // _WIN32
 
-/*abaixo toda as declarações de funções, estruturas, ponteiros e variáveis de auxílio*/
-
-void limpa_tela(){	/*irá limpar a tela a depender do sistema operacional*/
-	if (SISTEMAOPERACIONAL)
-		system("cls");
-	else
-		system("clear");
+void limpa_tela() {    /*irá limpar a tela a depender do sistema operacional*/
+    if (OPERATIONAL_SYSTEM)
+        system("cls");
+    else
+        system("clear");
 }
 
-void tecla_buff(){	/*irá limpar o buff no teclado a depender do sistema operacional*/
-	if (SISTEMAOPERACIONAL)
-		fflush(stdin);
-	else
-		__fpurge(stdin);
+void tecla_buff() {    /*irá limpar o buff no teclado a depender do sistema operacional*/
+    setbuf(stdin, NULL);
 }
 
-void entrada_buff(FILE *entrada){	/*irá limpar o buff do arquivo a depender do sistema operacional*/
-	if (SISTEMAOPERACIONAL)
-		fflush(entrada);
-	else
-		__fpurge(entrada);
+void entrada_buff(FILE *input) {    /*irá limpar o buff do arquivo a depender do sistema operacional*/
+    setbuf(input, NULL);
 }
--- src/padrao_nome.h
+++ src/padrao_nome.h
@@ -1,107 +1,102 @@
 #include <stdio.h>
 #include <string.h>
 
-int verifica_padrao(char nome_arquivo[], char padrao_nome[]){
-	
-	unsigned short int auxiliar;
-	
-	int quantidade_TOTAL = strlen(padrao_nome);
-	int quantidade_compara = strlen(nome_arquivo);
-	
-	char pra_comparar[strlen(nome_arquivo) + 1];
-	
-	if(quantidade_compara > quantidade_TOTAL){
-		int j = 0;
-		while(j < quantidade_TOTAL){
-			pra_comparar[j] = nome_arquivo[quantidade_compara - quantidade_TOTAL + j];
-			j = j + 1;
-		}
-	}
-	
-	else
-		strcpy(pra_comparar, nome_arquivo);
-	
-	int i = 0;
-	do{
-		
-		if( (pra_comparar[i] >= 48 && pra_comparar[i] <= 57) && (padrao_nome[i] >= 48 && padrao_nome[i] <= 57) )
-			auxiliar = 0;
-		else if(pra_comparar[i] == padrao_nome[i])
-			auxiliar = 0;
-		else
-			return 1;
-		i = i + 1;
-	}while(padrao_nome[i] != '\0');
+int verifica_padrao(char nome_arquivo[], char padrao_nome[]) {
 
-	return auxiliar;
+    unsigned short int auxiliar;
+
+    int quantidade_TOTAL = (int) strlen(padrao_nome);
+    int quantidade_compara = (int) strlen(nome_arquivo);
+
+    char pra_comparar[strlen(nome_arquivo) + 1];
+
+    if (quantidade_compara > quantidade_TOTAL) {
+        int j = 0;
+        while (j < quantidade_TOTAL) {
+            pra_comparar[j] = nome_arquivo[quantidade_compara - quantidade_TOTAL + j];
+            j = j + 1;
+        }
+    } else
+        strcpy(pra_comparar, nome_arquivo);
+
+    int i = 0;
+    do {
+
+        if ((pra_comparar[i] >= 48 && pra_comparar[i] <= 57) && (padrao_nome[i] >= 48 && padrao_nome[i] <= 57))
+            auxiliar = 0;
+        else if (pra_comparar[i] == padrao_nome[i])
+            auxiliar = 0;
+        else
+            return 1;
+        i = i + 1;
+    } while (padrao_nome[i] != '\0');
+
+    return auxiliar;
 }
 
-int verifica_padr_ate(char nome_arquivo[], char padrao_nome[], char termino){
-	
-	unsigned short int auxiliar;
-	
-	int quantidade_TOTAL = strlen(padrao_nome);
-	int quantidade_compara = strlen(nome_arquivo);
-	
-	char pra_comparar[strlen(nome_arquivo) + 1];
-	
-	if(quantidade_compara > quantidade_TOTAL){
-		int j = 0;
-		while(j < quantidade_TOTAL){
-			pra_comparar[j] = nome_arquivo[quantidade_compara - quantidade_TOTAL + j];
-			j = j + 1;
-		}
-	}
-	
-	else
-		strcpy(pra_comparar, nome_arquivo);
-	
-	int i = 0;
-	do{
-		
-		if( (pra_comparar[i] >= 48 && pra_comparar[i] <= 57) && (padrao_nome[i] >= 48 && padrao_nome[i] <= 57) )
-			auxiliar = 0;
-		else if(pra_comparar[i] == padrao_nome[i])
-			auxiliar = 0;
-		else
-			return 1;
-		i = i + 1;
-	}while(padrao_nome[i] != termino);
+int verifica_padr_ate(char nome_arquivo[], char padrao_nome[], char termino) {
 
-	return auxiliar;
+    unsigned short int auxiliar;
+
+    int quantidade_TOTAL = (int) strlen(padrao_nome);
+    int quantidade_compara = (int) strlen(nome_arquivo);
+
+    char pra_comparar[strlen(nome_arquivo) + 1];
+
+    if (quantidade_compara > quantidade_TOTAL) {
+        int j = 0;
+        while (j < quantidade_TOTAL) {
+            pra_comparar[j] = nome_arquivo[quantidade_compara - quantidade_TOTAL + j];
+            j = j + 1;
+        }
+    } else
+        strcpy(pra_comparar, nome_arquivo);
+
+    int i = 0;
+    do {
+
+        if ((pra_comparar[i] >= 48 && pra_comparar[i] <= 57) && (padrao_nome[i] >= 48 && padrao_nome[i] <= 57))
+            auxiliar = 0;
+        else if (pra_comparar[i] == padrao_nome[i])
+            auxiliar = 0;
+        else
+            return 1;
+        i = i + 1;
+    } while (padrao_nome[i] != termino);
+
+    return auxiliar;
 }
 
-unsigned short int nomainus(char nome_arquivo[], char padrao_nome[], char termino){
-	
-	unsigned short int auxiliar;
-	
-	int quantidade_TOTAL = strlen(padrao_nome);
-	int quantidade_compara = strlen(nome_arquivo);
-	
-	char pra_comparar[strlen(nome_arquivo) + 1];
-	
-	if(quantidade_compara > quantidade_TOTAL){
-		int j = 0;
-		while(j < quantidade_TOTAL){
-			pra_comparar[j] = nome_arquivo[quantidade_compara - quantidade_TOTAL + j];
-			j = j + 1;
-		}
-	}
-	
-	else
-		strcpy(pra_comparar, nome_arquivo);
-	
-	int i = 0;
-	do{
-		
-		if( (pra_comparar[i] >= 48 && pra_comparar[i] <= 57) && (padrao_nome[i] >= 48 && padrao_nome[i] <= 57) )
-			auxiliar = 0;
-		else if( (pra_comparar[i] == padrao_nome[i]) || (pra_comparar[i] == (padrao_nome[i] + 32)) || (pra_comparar[i] == (padrao_nome[i] - 32)) )
-			auxiliar = 0;
-		else
-			return 1;
-		i = i + 1;
-	}while(padrao_nome[i] != termino);
+unsigned short int nomainus(char nome_arquivo[], char padrao_nome[], char termino) {
 
-	return auxiliar;
+    unsigned short int auxiliar;
+
+    int quantidade_TOTAL = (int) strlen(padrao_nome);
+    int quantidade_compara = (int) strlen(nome_arquivo);
+
+    char pra_comparar[strlen(nome_arquivo) + 1];
+
+    if (quantidade_compara > quantidade_TOTAL) {
+        int j = 0;
+        while (j < quantidade_TOTAL) {
+            pra_comparar[j] = nome_arquivo[quantidade_compara - quantidade_TOTAL + j];
+            j = j + 1;
+        }
+    } else
+        strcpy(pra_comparar, nome_arquivo);
+
+    int i = 0;
+    do {
+
+        if ((pra_comparar[i] >= 48 && pra_comparar[i] <= 57) && (padrao_nome[i] >= 48 && padrao_nome[i] <= 57))
+            auxiliar = 0;
+        else if ((pra_comparar[i] == padrao_nome[i]) || (pra_comparar[i] == (padrao_nome[i] + 32)) ||
+                 (pra_comparar[i] == (padrao_nome[i] - 32)))
+            auxiliar = 0;
+        else
+            return 1;
+        i = i + 1;
+    } while (padrao_nome[i] != termino);
+
+    return auxiliar;
 }
